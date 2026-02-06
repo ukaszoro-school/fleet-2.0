c07d9b9a9d62c87b5ea2df9aba1881140c81ddc6
ukaszoro-school <lukasz.les32@student.pk.edu.pl>
2026-02-06 19:40:37 +0100

    feat(storage): added user struct with basic CRUD

--- server/storage.go
+++ server/storage.go
@@ -19,16 +19,14 @@ type Storage struct {
 	stopCollection  mongo.Collection
 }
 
-type Driver struct {
-	ID      string `json:"id"`
-	Name    string `json:"name"`
-	Surname string `json:"surname"`
-	Age     uint   `json:"age"`
-}
-
 type Stop struct {
 	ID       primitive.ObjectID `bson:"_id,omitempty"`
 	Location string             `bson:"location"`
+}
+
+type User struct {
+	ID   primitive.ObjectID `bson:"_id,omitempty"`
+	name string             `bson:"name"`
 }
 
 func storageNew(ctx context.Context, uri string) (*Storage, *mongo.Client) {
@@ -62,7 +60,7 @@ func (s *Storage) createStop(stop *Stop) (*mongo.InsertOneResult, error) {
 	return result, nil
 }
 
-func (s *Storage) GetAll() ([]Stop, error) {
+func (s *Storage) getAllStops() ([]Stop, error) {
 	var stops []Stop
 
 	cursor, err := s.stopCollection.Find(s.ctx, bson.M{})
@@ -86,7 +84,20 @@ func (s *Storage) GetAll() ([]Stop, error) {
 	return stops, nil
 }
 
-func (s *Storage) GetByLocation(location string) (*Stop, error) {
+func (s *Storage) getStopByID(id primitive.ObjectID) (*Stop, error) {
+	var stop Stop
+
+	filter := bson.M{"_id": id}
+
+	err := s.stopCollection.FindOne(s.ctx, filter).Decode(&stop)
+	if err != nil {
+		return nil, err
+	}
+
+	return &stop, nil
+}
+
+func (s *Storage) getStopByLocation(location string) (*Stop, error) {
 	var stop Stop
 
 	filter := bson.M{"location": location}
@@ -99,9 +110,61 @@ func (s *Storage) GetByLocation(location string) (*Stop, error) {
 	return &stop, nil
 }
 
-func (s *Storage) DeleteByID(id primitive.ObjectID) error {
+func (s *Storage) deleteStopByID(id primitive.ObjectID) error {
 	filter := bson.M{"_id": id}
 
 	_, err := s.stopCollection.DeleteOne(s.ctx, filter)
+	return err
+}
+
+func (s *Storage) createUser(user *User) (*mongo.InsertOneResult, error) {
+	result, err := s.stopCollection.InsertOne(s.ctx, user)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func (s *Storage) getAllUsers() ([]User, error) {
+	var users []User
+
+	cursor, err := s.userCollection.Find(s.ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(s.ctx)
+
+	for cursor.Next(s.ctx) {
+		var user User
+		if err := cursor.Decode(&user); err != nil {
+			return nil, err
+		}
+		users = append(users, user)
+	}
+
+	if err := cursor.Err(); err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
+func (s *Storage) getUserByID(id primitive.ObjectID) (*User, error) {
+	var user User
+
+	filter := bson.M{"_id": id}
+
+	err := s.userCollection.FindOne(s.ctx, filter).Decode(&user)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (s *Storage) deleteUserByID(id primitive.ObjectID) error {
+	filter := bson.M{"_id": id}
+
+	_, err := s.userCollection.DeleteOne(s.ctx, filter)
 	return err
 }
